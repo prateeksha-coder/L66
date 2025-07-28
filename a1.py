9dc8221cc44b3277826a497736ee7cb8349125e1
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-07-28 13:14:30 +0530

    initial commit

---
+++ a1.py
@@ -0,0 +1,13 @@
+def printPowerSet(set, set_size):
+    power_set_size = 2**set_size
+    outer = 0
+    inner = 0
+    for outer in range(0, power_set_size):
+        for inner in range()
+  
+    
+        
+
+
+  
+                                
