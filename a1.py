891dff4857e2201ca76411fbdffb545fdeab5af6
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-10-21 10:18:56 +0530

    commit

--- a1.py
+++ a1.py
@@ -8,6 +8,7 @@ def printPowerSet(set,set_size):
 	pow_set_size = (int) (math.pow(2, set_size))
 	counter = 0
 	j = 0
+	num=0
 	
 	for counter in range(0, pow_set_size):
 		for j in range(0, set_size):
@@ -15,9 +16,16 @@ def printPowerSet(set,set_size):
 			# Check if jth bit in the counter is set If set then print jth element from set
 			if((counter & (1 << j)) > 0):
 				print(set[j], end = "")
+		num+=1		
 		print("")
+		
+	print("size of power set: ",num)
+size=int(input("Enter array size: "))
+set=[]
+for i in range(0,size):
+	n=int(input("Enter element: "))
+	set.append(n)
 
-str = input("Enter string : ")
-printPowerSet(str, len(str))
+printPowerSet(set, len(set))
 
 
